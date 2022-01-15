2274fdb153646faecaba07bc7b9327610305b46e
AnushkaMadusanka <anushkamadusanka2016@gmail.com>
2022-01-15 17:04:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.coroutinesdemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.tvCount)
+        val countButton = findViewById<Button>(R.id.btnCount)
+        val downloadButton = findViewById<Button>(R.id.btnDownload)
+
+        countButton.setOnClickListener {
+            textView.text = count++.toString()
+        }
+        downloadButton.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                downloadUserData()
+            }
+
+        }
+    }
+}
+
+private fun downloadUserData() {
+    for (i in 1..200000) {
+        Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+    }
+}
